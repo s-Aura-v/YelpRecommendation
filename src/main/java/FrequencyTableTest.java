6f270204d4a23e6bf00088fe976a086e97f099cb
s-Aura-v <slamich2@oswego.edu>
2024-02-07 22:28:27 -0500

    tfDif Intro

--- src/main/java/FrequencyTableTest.java
+++ src/main/java/FrequencyTableTest.java
@@ -15,57 +15,59 @@ import java.util.Objects;
 public class FrequencyTableTest {
 
     public static void main(String[] args) throws FileNotFoundException {
-//        //Map - Key Value Pairs
-//        // key to value; string to int; what you recieve and what you want
-//        String[] arr = {"Hello", "gppd", "nice", "fuck", "fuck", "fuck", "gppd"};
-//        // HashMap< KeyType, DataType>
-//        HashMap<String, Integer> map = new HashMap<>();
-//        for (int i = 0; i < arr.length; i++) {
-//            if (map.containsKey(arr[i])) {
-//                map.put(arr[i], map.get(arr[i]) + 1);
-//            } else {
-//                map.put(arr[i], 1);
-//            }
-//        }
-//        for (Map.Entry entry: map.entrySet()) {
-//            System.out.println("Element | Frequency");
-//            System.out.println(entry.getKey() + " " + entry.getValue());
-//        }
-//        //Random method tests
-//        System.out.println(map.keySet() + " and " + map.values() + " and " + map.size());
-
-
-//        // Trying to use a file reader
-        //Bad Implementation - deprecated
-//        JsonParser parser = new JsonParser();
-//        FileReader reader = new FileReader("/Users/survive/Desktop/EEATO/24Spring/CSC365/FebProjectFiles/yelp_dataset/test.json");
-//
-//        Object obj = parser.parse(reader);
-//        System.out.println("Input: " + obj);
-//        JsonObject jsonObj = (JsonObject) obj;
-//        String categories = String.valueOf(jsonObj.get("categories"));
-//        System.out.println("Output: " + categories);
-//        String[] sortedCategories = categories.split(", ");
-////        for (String i : sortedCategories) {
-////            System.out.println("this is string " + i);
-////        }
-//        System.out.println(sortedCategories.length);
-//        for (int i = 0; i < sortedCategories.length; i++) {
-//            System.out.println(sortedCategories[i]);
-//        }
-
+        //Convert JSON to String to StringArray
         Gson gson = new Gson();
         FileReader reader = new FileReader("/Users/survive/Desktop/EEATO/24Spring/CSC365/FebProjectFiles/yelp_dataset/test.json");
         JsonObject categories = gson.fromJson(reader, JsonObject.class);
         String[] sortedCategories = String.valueOf(categories.get("categories")).split(", ");
-                for (String i : sortedCategories) {
-            System.out.println(i);
+        for (String i : sortedCategories) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+
+        // Convert String to Frequency
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < sortedCategories.length; i++) {
+            // If the map key exists, add 1 to its frequency
+            if (map.containsKey(sortedCategories[i])) {
+                map.put(sortedCategories[i], map.get(sortedCategories[i]) + 1);
+            } else {
+                // If the map key doesn't exist, set 1 to its frequency and add it to the map
+                map.put(sortedCategories[i], 1);
+            }
+        }
+        for (Map.Entry entry: map.entrySet()) {
+            System.out.println("Element: " + entry.getKey() + "\n| Frequency: " + entry.getValue());
+        }
+        System.out.println();
+
+        /*
+         TFIDF(t,d,D) = tf(t,d) * idf(t,D
+         tf(t,d) = n/N
+         n is the number of times term t appears in the document d.
+         N is the total number of terms in the document d.
+
+         idf(t,D) = log (N/( n))
+         N is the number of documents in the data set.
+         n is the number of documents that contain the term t among the data set.
+         */
+
+        // Finding tf(t,d)
+        HashMap<String, Double> termFrequency = new HashMap<>();
+        int size = sortedCategories.length;
+        for (int x = 0; x < sortedCategories.length; x++) {
+            //If Map has the string value, get it's value and divide it by total N and
+            if (map.containsKey(sortedCategories[x])) {
+                double freq = map.get(sortedCategories[x])/(Double.valueOf(size));
+                termFrequency.put(sortedCategories[x], freq);
+            }
+        }
+        for (Map.Entry entry: termFrequency.entrySet()) {
+            System.out.println("2: " + entry.getKey() + "\n| 2Freq: " + entry.getValue());
         }
 
-
-
-
-
+        //Find idf(t,D)
+        
 
     }
 }
