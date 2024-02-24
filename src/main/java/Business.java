67df2a418013b4dd71cda75907c1212dfc1e4c5d
s-Aura-v <slamich2@oswego.edu>
2024-02-23 20:34:37 -0500

    New Classes

---
+++ src/main/java/Business.java
@@ -0,0 +1,19 @@
+public class Business {
+    private String name;
+    private String review;
+
+    public Business(String review) {
+        this.review = review;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getReview() {
+        return review;
+    }
+
+
+}
---
+++ src/main/java/InformationRetrieval.java
@@ -0,0 +1,6 @@
+public class InformationRetrieval {
+    public static void main(String[] args) {
+        HT hashtable = new HT();
+        ;
+    }
+}
