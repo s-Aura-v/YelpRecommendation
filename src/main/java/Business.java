ffc7fb31889731d6a8f9a2eb6940bd9e428831c6
s-Aura-v <slamich2@oswego.edu>
2024-02-23 23:13:59 -0500

    Implementing HashTable instead...

--- src/main/java/Business.java
+++ src/main/java/Business.java
@@ -1,12 +1,25 @@
 public class Business {
     private String name;
     private String review;
+    private String id;
 
-    public Business(String review) {
-        this.review = review;
+    public Business(String name, String id) {
+        this.name = name;
+        this.id = id;
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getId() {
+        return id;
     }
     public String getName() {
         return name;
--- src/main/java/InformationRetrieval.java
+++ src/main/java/InformationRetrieval.java
@@ -1,6 +1,67 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class InformationRetrieval {
     public static void main(String[] args) {
         HT hashtable = new HT();
-        ;
+
+        Gson gson = new Gson();
+        BufferedReader buffRead;
+        JsonObject[] businessData = new JsonObject[4];
+        JsonObject[] businessReview = new JsonObject[4];
+
+        // 1. Get business name/id
+        int index = 0;
+        try {
+            buffRead = new BufferedReader(new FileReader("C:/Users/slamich2/Documents/GitHub/YelpRecommendation/src/main/java/jsonBusinessTest.json"));
+            while (index < businessData.length) {
+                String line = buffRead.readLine();
+                businessData[index] = gson.fromJson(line, JsonObject.class);
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < businessData.length; i++) {
+            String name = String.join(" ", String.valueOf(businessData[i].get("name")).split("\\P{Alnum}+"));
+            //Note: Might need to implement the substring in a better way
+            String id =  String.valueOf(businessData[i].get("business_id")).substring(1,23);
+            hashtable.add(new Business(name,id));
+            System.out.println(id + " " + name);
+        }
+
+        // 2. Get business id/reviews
+        index = 0;
+        try {
+            buffRead = new BufferedReader(new FileReader("C:/Users/slamich2/Documents/GitHub/YelpRecommendation/src/main/java/jsonReviewTest.json"));
+            while (index < businessData.length) {
+                String line = buffRead.readLine();
+                businessReview[index] = gson.fromJson(line, JsonObject.class);
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < businessReview.length; i++) {
+            String id = String.valueOf(businessReview[i].get("business_id")).substring(1,23);
+            String review = String.join(" ", String.valueOf(businessReview[i].get("text")).split("\\P{Alnum}+"));
+//            for ()
+        }
+
+        System.out.println(hashtable);
+
+
+        // 2. Get Business review and add it to the correct class
+
+
     }
+
+
+
 }
