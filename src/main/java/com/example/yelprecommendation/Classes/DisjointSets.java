a40d7800fcd8194db9ce2a8a98a8bff023330e28
s-Aura-v <slamich2@oswego.edu>
2024-04-17 22:01:07 -0400

    Randomization + Haversine Formula
    1. Some work on location
    2. Randomized Business

---
+++ src/main/java/com/example/yelprecommendation/Classes/DisjointSets.java
@@ -0,0 +1,4 @@
+package com.example.yelprecommendation.Classes;
+
+public class DisjointSets {
+}
--- src/main/java/com/example/yelprecommendation/Classes/InfoRetrieval.java
+++ src/main/java/com/example/yelprecommendation/Classes/InfoRetrieval.java
@@ -3,6 +3,7 @@ package com.example.yelprecommendation.Classes;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -11,6 +12,8 @@ import static java.lang.Double.MAX_VALUE;
 public class InfoRetrieval {
     static HashMap<String, Business> mapOfBusiness = new HashMap<>();
     static HashMap<String, String> businessNames = new HashMap<>();
+    // Latitude,Longitude
+    static HashMap<String, String> businessLocation = new HashMap<>();
     static List<Map.Entry<String, Double>> sortedScores;
     static HT frequencyTable = new HT();
     static int documentSize = 0;
@@ -25,8 +28,13 @@ public class InfoRetrieval {
             String line;
             while ((line = buffRead.readLine()) != null) {
                 JsonObject business = gson.fromJson(line, JsonObject.class);
+
                 String name = String.join(" ", String.valueOf(business.get("name")).split("[^a-zA-Z0-9'&]+")).substring(1);
-                String id =  String.valueOf(business.get("business_id")).substring(1,23);
+                String id = String.valueOf(business.get("business_id")).substring(1, 23);
+                String longitude = String.valueOf(business.get("longitude"));
+                String latitude = String.valueOf(business.get("latitude"));
+                String location = latitude + "," + longitude;
+                businessLocation.put(id, location);
                 businessNames.put(id, name);
                 documentSize++;
             }
@@ -51,7 +59,7 @@ public class InfoRetrieval {
         for (int i = 0; i < businessReview.length - 1; i++) {
             String id = String.valueOf(businessReview[i].get("business_id")).substring(1, 23);
 
-            String unfilteredReview = String.join( " ", String.valueOf(businessReview[i].get("text")).split("[^a-zA-Z0-9'&]+"));
+            String unfilteredReview = String.join(" ", String.valueOf(businessReview[i].get("text")).split("[^a-zA-Z0-9'&]+"));
             String review = "placeholder place";
             if (unfilteredReview.length() != 0) {
                 review = unfilteredReview.substring(1);
@@ -62,6 +70,10 @@ public class InfoRetrieval {
         for (String id : mapOfBusiness.keySet()) {
             if (businessNames.containsKey(id)) {
                 mapOfBusiness.get(id).setName(businessNames.get(id));
+                double latitude = Double.parseDouble(businessLocation.get(id).substring(0, businessLocation.get(id).indexOf(",")));
+                double longitude = Double.parseDouble(businessLocation.get(id).substring(businessLocation.get(id).indexOf(",") + 1));
+                mapOfBusiness.get(id).setLatitude(latitude);
+                mapOfBusiness.get(id).setLongitude(longitude);
             }
         }
 //        removeDuplicates();
@@ -105,7 +117,7 @@ public class InfoRetrieval {
             for (String word : termFrequency.keySet()) {
                 int term = termFrequency.get(word);
                 int size = termFrequency.size();
-                termFrequencyTF.put(word, (double) term/size);
+                termFrequencyTF.put(word, (double) term / size);
                 business.setTermFrequency(termFrequencyTF);
             }
         }
@@ -116,8 +128,8 @@ public class InfoRetrieval {
         for (Object x : frequencyTable) {
             String businessName = (String) x;
             int count = frequencyTable.getCount(x);
-            double idf = Math.log10( documentSize / (double) count);
-            idfValues.put(businessName,idf);
+            double idf = Math.log10(documentSize / (double) count);
+            idfValues.put(businessName, idf);
         }
 
         //6. TF-IDF : tf * idf
@@ -144,18 +156,19 @@ public class InfoRetrieval {
         System.out.println("Your file was serialized! :)");
 //        System.out.println(btree);
 //        System.out.println("height: "  + btree.height() + "\n size: " + btree.size() );
+
         return cosineSimilarity(inputtedID);
     }
 
-    public static BTree<String ,String> getBusinessBTree() {
-        BTree<String ,String> btree = new BTree();
+    public static BTree<String, String> getBusinessBTree() {
+        BTree<String, String> btree = new BTree();
         for (Business business : mapOfBusiness.values()) {
             btree.put(business.getName(), business.getId());
         }
         return btree;
     }
 
-     private static HashMap<String, Business> cosineSimilarity(String businessID) throws IOException {
+    private static HashMap<String, Business> cosineSimilarity(String businessID) throws IOException {
         // Cosine Similarity = (vector a * vector b) / (sqrt(vectorA^2) sqrt(vectorB^2))
         Business userInput = mapOfBusiness.get(businessID);
         HashMap<String, Double> similarityScores = new HashMap<>();
@@ -180,23 +193,28 @@ public class InfoRetrieval {
             business.setSimilarityValue(cosineSimilarity);
         }
         // Sort the similarity scores
-         HashMap<String, Double> scoresWithName = new HashMap<>();
-         for (String id : similarityScores.keySet()) {
-             mapOfBusiness.get(id).setSimilarityValue(similarityScores.get(id));
-             String name = businessNames.get(mapOfBusiness.get(id).getId());
-             scoresWithName.put(name, similarityScores.get(id));
-         }
-         sortedScores = new ArrayList<>(scoresWithName.entrySet());
-         sortedScores.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
-         findKMeans();
-         return mapOfBusiness;
+        HashMap<String, Double> scoresWithName = new HashMap<>();
+        for (String id : similarityScores.keySet()) {
+            mapOfBusiness.get(id).setSimilarityValue(similarityScores.get(id));
+            String name = businessNames.get(mapOfBusiness.get(id).getId());
+            scoresWithName.put(name, similarityScores.get(id));
+        }
+        sortedScores = new ArrayList<>(scoresWithName.entrySet());
+        sortedScores.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+        findKMeans();
+        randomBusinessGenerator(businessID);
+
+//        geographicCluster();
+
+
+        return mapOfBusiness;
     }
 
     public static List<Map.Entry<String, Double>> getSortedScores() {
         return sortedScores;
     }
 
-//    private static void removeDuplicates() {
+    //    private static void removeDuplicates() {
 //        HashMap<String, Business> uniqueBusinessess = new HashMap<>();
 //        for (Business business : mapOfBusiness.values()) {
 //            if (uniqueBusinessess.containsValue(business.getName())) {
@@ -368,6 +386,61 @@ public class InfoRetrieval {
         }
     }
 
+//    public static void geographicCluster() {
+//        // Control business is the name of the business that we'll try to find the four closest geographic location for
+//        Random r = new Random();
+//        HashMap<String, Double> closestBusiness = new HashMap<>();
+//        for (Business controlBusiness : mapOfBusiness.values() ) {
+//            double latDistance = 0;
+//            double longDistance = 0;
+//            closestBusiness.clear();
+//            for (Business experimentBusiness : mapOfBusiness.values()) {
+//                double controlLat = controlBusiness.getLatitude();
+//                double controlLong = controlBusiness.getLongitude();
+//                double experimentLat = experimentBusiness.getLatitude();
+//                double experimentLong = experimentBusiness.getLongitude();
+//
+//                // Radians
+//                latDistance = ( controlLat - experimentLat ) * (Math.PI / 180);
+//                longDistance = ( controlLong - experimentLong ) * (Math.PI / 180);
+//                controlLat = (controlLat) * (Math.PI / 180);
+//                experimentLat = (experimentLat) * (Math.PI / 180);
+//
+//                //Apply Formula
+//                double a = Math.pow(Math.sin(latDistance / 2), 2) +
+//                        Math.pow(Math.sin(longDistance / 2), 2) *
+//                                Math.cos(controlLat) * Math.cos(experimentLat);
+//                double rad = 6371;
+//                double c = 2 * Math.asin(Math.sqrt(a));
+//                double distance = c * rad;
+//                closestBusiness.put(experimentBusiness.getId(), distance);
+//            }
+////            ArrayList<Map.Entry<String, Double>> closestBusinessSorted = new ArrayList<>(closestBusiness.entrySet());
+////            closestBusinessSorted.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+////            for (int i = 0; i < 4; i++) {
+////                controlBusiness.setClosestBusiness(closestBusinessSorted.get(r.nextInt()), distance);
+//            }
+//
+//            for (Business x : mapOfBusiness.values()) {
+//                System.out.println(x.getClosestBusiness());
+//            }
+//        }
+
+    /*
+    Generate 1000 businesses that will be sorted into the closest 4
+    Used for randomness. Don't want all business to be from the same location
+
+    Note: These shuffle the business every time. Maybe I should keep them the same.
+     */
+    public static void randomBusinessGenerator(String id) {
+        Random r = new Random(System.currentTimeMillis());
+        List<Business> shuffledBusiness = new ArrayList<Business>(mapOfBusiness.values());
+        Collections.shuffle(shuffledBusiness);
+        String[] randomizedBusiness =  new String[1000];
+        for (int i = 0; i < 1000; i++) {
+            randomizedBusiness[i] = shuffledBusiness.get(i).getId();
+            System.out.println(randomizedBusiness[i]);
+        }
+    }
+
 }
-
-
